f691ca835eec10407275c22e2cba57429a71c823
José Beltrán Solís <josebsx@live.com.mx>
2019-02-11 19:21:01 -0600

    Add composer install

--- build.sh
+++ build.sh
@@ -6,5 +6,12 @@ fi
 echo Remove .DS_Store files
 find . -name ".DS_Store" -delete
 
+if [ -f compropago.zip ]; then
+    echo
+else
+    echo Install requirements
+    composer install
+fi
+
 echo "Building tgz module for Magento"
 zip -r compropago.zip . -x "*.git*" "*.idea*" ".DS_Store" "build.sh"
